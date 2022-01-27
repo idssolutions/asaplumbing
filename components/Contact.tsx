bcbf58fff2f57577b82cf6fa99865f4b9500ce78
Ira Sanchez <irasanchez@Iras-MacBook-Air.local>
2022-01-26 19:53:21 -0700

    Controlled input added

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 export default function Contact() {
+  const [contactData, setContactData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [submitted, setSubmitted] = useState(false);
+  const handleChange = ({ target }) => {
+    setContactData({ ...contactData, [target.name]: target.value });
+  };
   return (
     <section className="py-20">
       <div className="container px-4 mx-auto">
@@ -9,33 +18,39 @@ export default function Contact() {
               <h2 className="mb-8 text-3xl font-bold leading-tight lg:mb-12 md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-heading">
                 Questions, Concerns, Need a Quote?
               </h2>
-              <h3 className="mb-1 text-2xl font-bold font-heading">
-                Address
-              </h3>
+              <h3 className="mb-1 text-2xl font-bold font-heading">Address</h3>
               <address className="text-lg leading-loose text-blue-500">
                 1228 E. McLellan Blvd. Phoenix, AZ 85014
               </address>
-              <h3 className="mb-1 text-2xl font-bold font-heading">
-                Phone
-              </h3>
+              <h3 className="mb-1 text-2xl font-bold font-heading">Phone</h3>
               <p className="text-lg leading-loose text-blue-500">
                 (480) 796-4060
               </p>
             </div>
           </div>
           <div className="w-full px-4 lg:w-1/2">
-            <form action="#">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                console.log(contactData);
+              }}
+            >
               <input
+                onChange={handleChange}
                 className="w-full py-3 pl-3 mb-4 border rounded"
                 type="text"
+                name="name"
                 placeholder="Name"
               />
               <input
+                onChange={handleChange}
                 className="w-full py-3 pl-3 mb-4 border rounded"
                 type="email"
+                name="email"
                 placeholder="E-mail"
               />
               <textarea
+                onChange={handleChange}
                 className="w-full p-3 mb-4 border rounded resize-none"
                 name="message"
                 cols={30}
